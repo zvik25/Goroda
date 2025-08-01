328062f842bc2d1410968dd7f3067e1d8a0562ca
zvik25 <zvik@mail.ru>
2025-08-01 23:16:44 +0300

    Добавлен ввод с помощью клавиши Enter.

--- Main.py
+++ Main.py
@@ -9,30 +9,30 @@ def get_coordinates(city, key):
         if results:
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
-            return f'Широта: {lat}, Доглота: {lon}'
+            return f'Широта: {lat}, Долгота: {lon}'
         else:
-            return 'Город не найден'
+            return 'Город не найден.'
     except Exception as e:
-        return f'Возникла ошибка:  {e}'
+        return f'Возникла ошибка: {e}'
 
 
-def show_coordinates():
-    city = entry.gat()
+def show_coordinates(event=None):
+    city = entry.get()
     coordinates = get_coordinates(city, key)
-    label.config(text=f'Координаты города {city}: {coordinates}')
+    label.config(text=f'Координаты города {city}:\n {coordinates}')
 
 
 key = '585e58759bff461d93c336a542e525b6'
 
-
 window = Tk()
 window.title('Координаты городов')
-window.geometry('200x100')
+window.geometry('320x100')
 
 entry = Entry()
 entry.pack()
+entry.bind('<Return>', show_coordinates)
 
-button = Button(text='Поиск координат', command=get_coordinates)
+button = Button(text='Поиск координат', command=show_coordinates)
 button.pack()
 
 label = Label(text='Введите город и нажмите на кнопку')
